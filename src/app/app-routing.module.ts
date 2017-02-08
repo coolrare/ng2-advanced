b1a5c44c042952be90036760090f88343a704bc5
Will 保哥 <doggy.huang@gmail.com>
2017-02-08 17:12:02 +0800

    實作簡單的第一層路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: LayoutComponent
   }
 ];
 
