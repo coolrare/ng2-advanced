1def8b79817a760896c29a79a6fa94cdda6f8af7
Will 保哥 <doggy.huang@gmail.com>
2017-02-10 22:27:38 +0800

    示範在路由之間傳遞可選的路由查詢參數 (Optional Parameters)

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -11,6 +11,8 @@ export class CardsComponent implements OnInit {
 
   type: string;
 
+  ts: number;
+
   constructor(private router: Router,
               private route: ActivatedRoute) { }
 
@@ -22,10 +24,18 @@ export class CardsComponent implements OnInit {
       this.type = params['type'];
     });
 
+    // this.ts = +this.route.snapshot.queryParams['ts'];
+
+    this.route.queryParams.subscribe((params: Params) => {
+      this.ts = params['ts'];
+    })
+
   }
 
   goType(type: string) {
-    this.router.navigate(['cards', type, { ts: new Date().getTime() }]);
+    this.router.navigate(['cards', type], {
+      queryParams: { ts: new Date().getTime() }
+    });
     // this.router.navigate(['../' + type], { relativeTo: this.route });
     // this.router.navigateByUrl('/cards/' + type);
   }
