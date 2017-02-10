89dad19fd51500055c82f4e289f4a2c7cd40630a
Will 保哥 <doggy.huang@gmail.com>
2017-02-10 22:44:02 +0800

    建立含路由的 charts 模組
    手動刪除多餘的 charts.component.* 檔案

---
+++ src/app/charts/charts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartsRoutingModule } from './charts-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ChartsRoutingModule
+  ],
+  declarations: [
+
+  ]
+})
+export class ChartsModule { }
