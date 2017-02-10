4078c20d05f458c76c26b16ef6639744d409258d
Will 保哥 <doggy.huang@gmail.com>
2017-02-10 22:55:12 +0800

    將屬於 charts 相關的路由與子路由都移至 charts-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,14 @@
-import { RickshawComponent } from './charts/rickshaw/rickshaw.component';
-import { RadialComponent } from './charts/radial/radial.component';
-import { FlotComponent } from './charts/flot/flot.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CardsComponent } from './cards/cards.component';
 import { NgModule } from '@angular/core';
 import { Route, Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts',
-    children: [
-      { path: '', redirectTo: 'flot', pathMatch: 'full' },
-      { path: 'flot', component: FlotComponent },
-      { path: 'radial', component: RadialComponent },
-      { path: 'rickshaw', component: RickshawComponent }
-    ]
-  },
   fallbackRoute
 ];
 
--- src/app/charts/charts-routing.module.ts
+++ src/app/charts/charts-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RickshawComponent } from './rickshaw/rickshaw.component';
+import { RadialComponent } from './radial/radial.component';
+import { FlotComponent } from './flot/flot.component';
+
+const routes: Routes = [
+  { path: 'charts',
+    children: [
+      { path: '', redirectTo: 'flot', pathMatch: 'full' },
+      { path: 'flot', component: FlotComponent },
+      { path: 'radial', component: RadialComponent },
+      { path: 'rickshaw', component: RickshawComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
